095ddefd19a1a2c40d6cc9e707114470391e4df7
Mayen Akpan <neroxma@yahoo.com>
2022-01-04 20:14:06 -0800

    Updated function

--- script.js
+++ script.js
@@ -10,26 +10,38 @@ const apiUrl = `https://api.unsplash.com/photos/random
 /?client_id=${apiKey}&count=${count}
 `;
 
+//helper function
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // create elements for links and photos add to dom
 function displayPhotos() {
   //run function fr each object in photoarray
   photosArray.forEach((photo) => {
     // create <a> to link to unspash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
-    //craete img for photo
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    //create img for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     //put img inside a,
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
 }
 
-//get photos from unspalsh api
+//get photos from unsplash api
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
